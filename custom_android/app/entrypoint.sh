e03759923c22113cdc97ab27a6c487f812cb3c63
Nicola Valentino MESSUTI <nicola.messuti@alten.it>
2023-02-16 16:04:27 +0100

    add android

---
+++ custom_android/app/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
+RUN_TERMINAL=${RUN_TERMINAL:-yes}
+
+case $RUN_FLUXBOX in
+  false|no|n|0)
+    rm -f /app/config/fluxbox.conf
+    ;;
+esac
+
+case $RUN_TERMINAl in
+  false|no|n|0)
+    rm -f /app/config/xterm.conf
+    ;;
+esac
+
+exec supervisord -c /app/supervisord.conf
+
